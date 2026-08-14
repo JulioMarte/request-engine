95786d220c8dce5e680ca0fff6d5aa2d0cf4a14a
JulioMarte <julio19955@hotmail.com>
2026-08-14 06:54:59 -0400

    Use module-owned worker connection surfaces

--- src/request_engine/entrypoints/worker/scheduled_runtime.py
+++ src/request_engine/entrypoints/worker/scheduled_runtime.py
@@ -1,26 +1,20 @@
 from request_engine.entrypoints.worker.scheduled_router import ScheduledActionRouter
-from request_engine.modules.booking.adapters.db.lifecycle_scheduling import (
+from request_engine.modules.booking.worker import (
     NO_SHOW_ACTION_TYPE,
     NO_SHOW_ACTION_VERSION,
+    NoShowScheduledHandler,
 )
-from request_engine.modules.booking.adapters.worker.no_show import NoShowScheduledHandler
-from request_engine.modules.communications.adapters.db.delivery_store import (
+from request_engine.modules.communications.worker import (
     DISPATCH_ACTION_TYPE,
     DISPATCH_ACTION_VERSION,
     RECONCILE_ACTION_TYPE,
     RECONCILE_ACTION_VERSION,
-)
-from request_engine.modules.communications.adapters.db.reminder_commands import (
     REMINDER_ACTION_TYPE,
     REMINDER_ACTION_VERSION,
-)
-from request_engine.modules.communications.adapters.db.reminder_occurrences import (
+    CommunicationDeliveryScheduledHandler,
     PostgresReminderOccurrenceCommands,
 )
-from request_engine.modules.communications.adapters.worker.scheduled_delivery import (
-    CommunicationDeliveryScheduledHandler,
-)
-from request_engine.modules.queue.adapters.worker.slot_offer_expiry import (
+from request_engine.modules.queue.worker import (
     SLOT_OFFER_EXPIRY_ACTION_TYPE,
     SLOT_OFFER_EXPIRY_ACTION_VERSION,
     SlotOfferExpiryScheduledHandler,
