187dd60dfd0708776b62e635a3efd6b9e792d3c3
JulioMarte <julio19955@hotmail.com>
2026-08-14 06:54:36 -0400

    Publish queue worker connection surface

---
+++ src/request_engine/modules/queue/worker.py
@@ -0,0 +1,11 @@
+from request_engine.modules.queue.adapters.worker.slot_offer_expiry import (
+    SLOT_OFFER_EXPIRY_ACTION_TYPE,
+    SLOT_OFFER_EXPIRY_ACTION_VERSION,
+    SlotOfferExpiryScheduledHandler,
+)
+
+__all__ = [
+    "SLOT_OFFER_EXPIRY_ACTION_TYPE",
+    "SLOT_OFFER_EXPIRY_ACTION_VERSION",
+    "SlotOfferExpiryScheduledHandler",
+]
