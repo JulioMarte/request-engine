390df29e80260b43c74469cc3fe550d0d06bc87f
JulioMarte <julio19955@hotmail.com>
2026-08-19 06:10:09 -0400

    test: expose G18 validator to manifest import harness

---
+++ validate_v3_adversarial_failure_artifact.py
@@ -0,0 +1,5 @@
+"""Import shim for tooling that loads the release manifest builder as a module."""
+
+from scripts.release.validate_v3_adversarial_failure_artifact import validate_adversarial_failure
+
+__all__ = ["validate_adversarial_failure"]
