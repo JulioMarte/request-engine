01c9c41edb4e3305af1a4bacd22539551afb8104
JulioMarte <julio19955@hotmail.com>
2026-08-19 06:18:45 -0400

    docs: reconcile G18 race proof and remove import shim

--- validate_v3_adversarial_failure_artifact.py
+++
@@ -1,5 +0,0 @@
-"""Import shim for tooling that loads the release manifest builder as a module."""
-
-from scripts.release.validate_v3_adversarial_failure_artifact import validate_adversarial_failure
-
-__all__ = ["validate_adversarial_failure"]
