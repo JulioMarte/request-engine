2b309b34f09f9c1afbf28627a4dfcd8ed6b4752a
JulioMarte <julio19955@hotmail.com>
2026-08-22 18:38:33 -0400

    feat: install discovery operational module

--- src/request_engine/entrypoints/http/operational_composition.py
+++ src/request_engine/entrypoints/http/operational_composition.py
@@ -6,6 +6,9 @@ from request_engine.modules.booking.api import (
 from request_engine.modules.catalog.api import (
     install_operational_http as install_catalog_operational_http,
 )
+from request_engine.modules.discovery.api import (
+    install_operational_http as install_discovery_operational_http,
+)
 from request_engine.modules.tenancy.api import (
     install_operational_http as install_tenancy_operational_http,
 )
@@ -32,6 +35,11 @@ def install_operational_modules(
         actor_resolver=actor_resolver,
     )
     install_booking_operational_http(
+        app,
+        session_factory=session_factory,
+        actor_resolver=actor_resolver,
+    )
+    install_discovery_operational_http(
         app,
         session_factory=session_factory,
         actor_resolver=actor_resolver,
