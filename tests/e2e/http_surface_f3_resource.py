c5541ff37b0acedbdf9776174fa39b9281a2f5bb
JulioMarte <julio19955@hotmail.com>
2026-08-25 16:24:33 -0400

    test(f3): classify resource activity read surface

--- tests/e2e/http_surface_f3_resource.py
+++ tests/e2e/http_surface_f3_resource.py
@@ -4,6 +4,16 @@ P1 = "00000000-0000-4000-8000-000000000001"
 
 F3_RESOURCE_OPERATIONS: tuple[PublicHttpOperation, ...] = (
     PublicHttpOperation(
+        "resource_activity.read",
+        "GET",
+        "/v1/resource-activities",
+        "resource_activity.read",
+        False,
+        False,
+        TenantIsolationMode.FILTERED,
+        HttpProbe(f"/v1/resource-activities?resource_id={P1}"),
+    ),
+    PublicHttpOperation(
         "resource_activity.start",
         "POST",
         "/v1/resource-activities",
