f1680726448aac5f1912d326c39aea8ca39ae4a5
JulioMarte <julio19955@hotmail.com>
2026-08-25 11:49:45 -0400

    Apply canonical format to F3 HTTP facade

--- tests/e2e/http_surface_f3.py
+++ tests/e2e/http_surface_f3.py
@@ -2,6 +2,4 @@ from .http_surface import PublicHttpOperation
 from .http_surface_f3_resource import F3_RESOURCE_OPERATIONS
 from .http_surface_f3_service import F3_SERVICE_OPERATIONS
 
-F3_HTTP_OPERATIONS: tuple[PublicHttpOperation, ...] = (
-    F3_SERVICE_OPERATIONS + F3_RESOURCE_OPERATIONS
-)
+F3_HTTP_OPERATIONS: tuple[PublicHttpOperation, ...] = F3_SERVICE_OPERATIONS + F3_RESOURCE_OPERATIONS
