ccdc7d79eb98f865f462addeccb2a14d35235c2f
JulioMarte <julio19955@hotmail.com>
2026-08-26 15:47:21 -0400

    Include F4 in current HTTP surface registry

--- tests/e2e/http_surface_current.py
+++ tests/e2e/http_surface_current.py
@@ -1,8 +1,11 @@
 from .http_surface import PUBLIC_HTTP_OPERATIONS as V3_HTTP_OPERATIONS
 from .http_surface import PublicHttpOperation
 from .http_surface_f3 import F3_HTTP_OPERATIONS
+from .http_surface_f4 import F4_HTTP_OPERATIONS
 
-PUBLIC_HTTP_OPERATIONS: tuple[PublicHttpOperation, ...] = V3_HTTP_OPERATIONS + F3_HTTP_OPERATIONS
+PUBLIC_HTTP_OPERATIONS: tuple[PublicHttpOperation, ...] = (
+    V3_HTTP_OPERATIONS + F3_HTTP_OPERATIONS + F4_HTTP_OPERATIONS
+)
 
 
 def operation_keys() -> frozenset[tuple[str, str]]:
