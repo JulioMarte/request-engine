da82a02e88a964c01df423c08433fa19713c0b13
JulioMarte <julio19955@hotmail.com>
2026-08-26 22:34:45 -0400

    Add F5 to live capability registry

--- src/request_engine/platform/security/capability_registry_live.py
+++ src/request_engine/platform/security/capability_registry_live.py
@@ -4,11 +4,17 @@ from request_engine.platform.security.capability_registry_delivery_live import (
 from request_engine.platform.security.capability_registry_live_capacity import (
     LIVE_CAPACITY_CAPABILITIES,
 )
+from request_engine.platform.security.capability_registry_operational_recovery import (
+    OPERATIONAL_RECOVERY_CAPABILITIES,
+)
 from request_engine.platform.security.capability_registry_queue_live import (
     LIVE_QUEUE_CAPABILITIES,
 )
 from request_engine.platform.security.capability_types import CapabilityDefinition
 
 LIVE_CAPABILITIES: tuple[CapabilityDefinition, ...] = (
-    LIVE_QUEUE_CAPABILITIES + LIVE_DELIVERY_CAPABILITIES + LIVE_CAPACITY_CAPABILITIES
+    LIVE_QUEUE_CAPABILITIES
+    + LIVE_DELIVERY_CAPABILITIES
+    + LIVE_CAPACITY_CAPABILITIES
+    + OPERATIONAL_RECOVERY_CAPABILITIES
 )
