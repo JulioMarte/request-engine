6bf9808095987bd55dab0766a4a3c218811102f8
JulioMarte <julio19955@hotmail.com>
2026-08-30 18:00:59 -0400

    Use extracted F6 HTTP composition helper

--- src/request_engine/entrypoints/http/module_composition.py
+++ src/request_engine/entrypoints/http/module_composition.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from request_engine.bootstrap.recovery_catalog import CatalogRecoveryLocationAdapter
 from request_engine.bootstrap.recovery_queue import QueueRecoveryIntakeAdapter
+from request_engine.entrypoints.http.copilot_composition import install_operational_copilot
 from request_engine.modules.booking.api import install_http as install_booking_http
 from request_engine.modules.booking.api.live_capacity import (
     build_live_capacity_source as build_booking_live_capacity_source,
@@ -15,25 +16,14 @@ from request_engine.modules.catalog.api.recovery_schedule import (
     build_recovery_location_schedule_port,
 )
 from request_engine.modules.communications.api import install_http as install_communications_http
-from request_engine.modules.communications.api.recovery import (
-    build_recovery_communication_port,
-)
+from request_engine.modules.communications.api.recovery import build_recovery_communication_port
 from request_engine.modules.delivery.api import install_http as install_delivery_http
 from request_engine.modules.delivery.api.live_capacity import (
     build_live_capacity_source as build_delivery_live_capacity_source,
 )
 from request_engine.modules.live_capacity.api import install_http as install_live_capacity_http
 from request_engine.modules.live_capacity.api.recovery import build_recovery_capacity_source
-from request_engine.modules.operational_copilot.api import (
-    build_live_capacity_at_risk_reader,
-)
-from request_engine.modules.operational_copilot.api import (
-    install_http as install_copilot_http,
-)
 from request_engine.modules.operational_recovery.api import install_http as install_recovery_http
-from request_engine.modules.operational_recovery.api.proposal_reader import (
-    build_recovery_proposal_reader,
-)
 from request_engine.modules.queue.api import QueueSlotOfferHttpPorts
 from request_engine.modules.queue.api import install_http as install_queue_http
 from request_engine.modules.queue.api.live_capacity import (
@@ -41,10 +31,7 @@ from request_engine.modules.queue.api.live_capacity import (
 )
 from request_engine.modules.queue.api.recovery import build_recovery_intake_control_port
 from request_engine.modules.requests.api import install_http as install_requests_http
-from request_engine.modules.tenancy.api import (
-    build_operational_authority_party_reader,
-    build_party_authority_reader,
-)
+from request_engine.modules.tenancy.api import build_party_authority_reader
 from request_engine.platform.db.session import SessionFactory
 from request_engine.platform.security.http import ActorResolver
 
@@ -113,12 +100,11 @@ def install_business_modules(
         location_schedule=CatalogRecoveryLocationAdapter(location_schedule),
         assignment_schedule=build_recovery_assignment_schedule_port(session_factory),
     )
-    install_copilot_http(
+    install_operational_copilot(
         app,
+        session_factory=session_factory,
         actor_resolver=actor_resolver,
-        at_risk_reader=build_live_capacity_at_risk_reader(recovery_capacity),
-        proposal_reader=build_recovery_proposal_reader(recovery_service),
-        authority_reader=build_operational_authority_party_reader(session_factory),
-        intake_executor=recovery_workflow,
-        extend_day_executor=recovery_workflow,
+        recovery_capacity=recovery_capacity,
+        recovery_service=recovery_service,
+        recovery_workflow=recovery_workflow,
     )
