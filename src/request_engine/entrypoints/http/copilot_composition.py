36f444aa55f900536177f57cc01e8fea867dee2d
JulioMarte <julio19955@hotmail.com>
2026-08-30 18:04:20 -0400

    Simplify F6 composition imports

--- src/request_engine/entrypoints/http/copilot_composition.py
+++ src/request_engine/entrypoints/http/copilot_composition.py
@@ -2,24 +2,14 @@ from typing import Protocol
 
 from fastapi import FastAPI
 
-from request_engine.modules.live_capacity.contracts.recovery import (
-    RecoveryCapacitySource,
-)
-from request_engine.modules.operational_copilot.api import (
-    build_live_capacity_at_risk_reader,
-    install_http as install_copilot_http,
-)
+from request_engine.modules.live_capacity.contracts.recovery import RecoveryCapacitySource
+from request_engine.modules.operational_copilot import api as operational_copilot_api
 from request_engine.modules.operational_copilot.application.ports import (
     RecoveryExtendDayExecutor,
     RecoveryIntakeExecutor,
     RecoveryProposalReader,
 )
-from request_engine.modules.operational_recovery.api.proposal_reader import (
-    build_recovery_proposal_reader,
-)
-from request_engine.modules.tenancy.api import (
-    build_operational_authority_party_reader,
-)
+from request_engine.modules.tenancy import api as tenancy_api
 from request_engine.platform.db.session import SessionFactory
 from request_engine.platform.security.http import ActorResolver
 
@@ -41,12 +31,14 @@ def install_operational_copilot(
     recovery_service: RecoveryProposalReader,
     recovery_workflow: RecoveryWorkflowExecutor,
 ) -> None:
-    install_copilot_http(
+    operational_copilot_api.install_http(
         app,
         actor_resolver=actor_resolver,
-        at_risk_reader=build_live_capacity_at_risk_reader(recovery_capacity),
-        proposal_reader=build_recovery_proposal_reader(recovery_service),
-        authority_reader=build_operational_authority_party_reader(session_factory),
+        at_risk_reader=operational_copilot_api.build_live_capacity_at_risk_reader(
+            recovery_capacity
+        ),
+        proposal_reader=recovery_service,
+        authority_reader=tenancy_api.build_operational_authority_party_reader(session_factory),
         intake_executor=recovery_workflow,
         extend_day_executor=recovery_workflow,
     )
