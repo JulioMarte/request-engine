73fd17250af4866ebed89f726c4764bc5675f3cb
JulioMarte <julio19955@hotmail.com>
2026-08-30 18:08:17 -0400

    Align F6 composition imports with repository convention

--- src/request_engine/entrypoints/http/module_composition.py
+++ src/request_engine/entrypoints/http/module_composition.py
@@ -15,31 +15,27 @@ from request_engine.modules.catalog.api.recovery_schedule import (
     build_recovery_location_schedule_port,
 )
 from request_engine.modules.communications.api import install_http as install_communications_http
-from request_engine.modules.communications.api.recovery import build_recovery_communication_port
+from request_engine.modules.communications.api.recovery import (
+    build_recovery_communication_port,
+)
 from request_engine.modules.delivery.api import install_http as install_delivery_http
 from request_engine.modules.delivery.api.live_capacity import (
     build_live_capacity_source as build_delivery_live_capacity_source,
 )
 from request_engine.modules.live_capacity.api import install_http as install_live_capacity_http
 from request_engine.modules.live_capacity.api.recovery import build_recovery_capacity_source
-from request_engine.modules.operational_copilot.api import (
-    build_live_capacity_at_risk_reader,
-    install_http as install_copilot_http,
-)
+from request_engine.modules.operational_copilot.api import build_live_capacity_at_risk_reader
+from request_engine.modules.operational_copilot.api import install_http as install_copilot_http
 from request_engine.modules.operational_recovery.api import install_http as install_recovery_http
-from request_engine.modules.queue.api import (
-    QueueSlotOfferHttpPorts,
-    install_http as install_queue_http,
-)
+from request_engine.modules.queue.api import QueueSlotOfferHttpPorts
+from request_engine.modules.queue.api import install_http as install_queue_http
 from request_engine.modules.queue.api.live_capacity import (
     build_live_capacity_source as build_queue_live_capacity_source,
 )
 from request_engine.modules.queue.api.recovery import build_recovery_intake_control_port
 from request_engine.modules.requests.api import install_http as install_requests_http
-from request_engine.modules.tenancy.api import (
-    build_operational_authority_party_reader,
-    build_party_authority_reader,
-)
+from request_engine.modules.tenancy.api import build_operational_authority_party_reader
+from request_engine.modules.tenancy.api import build_party_authority_reader
 from request_engine.platform.db.session import SessionFactory
 from request_engine.platform.security.http import ActorResolver
 
