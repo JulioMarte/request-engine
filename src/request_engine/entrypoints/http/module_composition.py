efdaeb3d038f243d8ca0a7fe8034207c3ffb3f24
JulioMarte <julio19955@hotmail.com>
2026-08-30 18:31:39 -0400

    Wire complete F6 mutation execution surfaces

--- src/request_engine/entrypoints/http/module_composition.py
+++ src/request_engine/entrypoints/http/module_composition.py
@@ -7,20 +7,17 @@ from request_engine.modules.booking.api.live_capacity import (
     build_live_capacity_source as build_booking_live_capacity_source,
 )
 from request_engine.modules.booking.api.recovery import build_recovery_booking_port
-from request_engine.modules.booking.api.recovery_schedule import (
-    build_recovery_assignment_schedule_port,
-)
+from request_engine.modules.booking.api.recovery_schedule import build_recovery_assignment_schedule_port
 from request_engine.modules.catalog.api import install_http as install_catalog_http
-from request_engine.modules.catalog.api.recovery_schedule import (
-    build_recovery_location_schedule_port,
-)
+from request_engine.modules.catalog.api.recovery_schedule import build_recovery_location_schedule_port
 from request_engine.modules.communications.api import install_http as install_communications_http
-from request_engine.modules.communications.api.recovery import (
-    build_recovery_communication_port,
-)
+from request_engine.modules.communications.api.recovery import build_recovery_communication_port
 from request_engine.modules.delivery.api import install_http as install_delivery_http
 from request_engine.modules.delivery.api.live_capacity import (
     build_live_capacity_source as build_delivery_live_capacity_source,
+)
+from request_engine.modules.discovery.api.publication_runtime import (
+    build_discovery_publication_runtime,
 )
 from request_engine.modules.live_capacity.api import install_http as install_live_capacity_http
 from request_engine.modules.live_capacity.api.recovery import build_recovery_capacity_source
@@ -66,11 +63,7 @@ def install_business_modules(
         actor_resolver=actor_resolver,
         slot_offer_ports=slot_offer_ports,
     )
-    install_delivery_http(
-        app,
-        session_factory=session_factory,
-        actor_resolver=actor_resolver,
-    )
+    install_delivery_http(app, session_factory=session_factory, actor_resolver=actor_resolver)
     booking_capacity = build_booking_live_capacity_source()
     queue_capacity = build_queue_live_capacity_source()
     delivery_capacity = build_delivery_live_capacity_source()
@@ -82,36 +75,34 @@ def install_business_modules(
         queue_source=queue_capacity,
         delivery_source=delivery_capacity,
     )
-    install_communications_http(
-        app,
-        session_factory=session_factory,
-        actor_resolver=actor_resolver,
-    )
-    queue_intake = build_recovery_intake_control_port(session_factory)
-    location_schedule = build_recovery_location_schedule_port(session_factory)
+    install_communications_http(app, session_factory=session_factory, actor_resolver=actor_resolver)
     recovery_capacity = build_recovery_capacity_source(
         session_factory,
         booking_source=booking_capacity,
         queue_source=queue_capacity,
         delivery_source=delivery_capacity,
     )
-    recovery_service, recovery_workflow = install_recovery_http(
+    recovery = install_recovery_http(
         app,
         session_factory=session_factory,
         actor_resolver=actor_resolver,
         capacity=recovery_capacity,
         booking=build_recovery_booking_port(session_factory),
         communications=build_recovery_communication_port(session_factory),
-        intake=QueueRecoveryIntakeAdapter(queue_intake),
-        location_schedule=CatalogRecoveryLocationAdapter(location_schedule),
+        intake=QueueRecoveryIntakeAdapter(build_recovery_intake_control_port(session_factory)),
+        location_schedule=CatalogRecoveryLocationAdapter(
+            build_recovery_location_schedule_port(session_factory)
+        ),
         assignment_schedule=build_recovery_assignment_schedule_port(session_factory),
     )
     install_copilot_http(
         app,
         actor_resolver=actor_resolver,
         at_risk_reader=build_live_capacity_at_risk_reader(recovery_capacity),
-        proposal_reader=recovery_service,
+        proposal_reader=recovery.service,
         authority_reader=build_operational_authority_party_reader(session_factory),
-        intake_executor=recovery_workflow,
-        extend_day_executor=recovery_workflow,
+        recovery_executor=recovery.service,
+        intake_executor=recovery.workflow,
+        extend_day_executor=recovery.workflow,
+        discovery_executor=build_discovery_publication_runtime(session_factory),
     )
