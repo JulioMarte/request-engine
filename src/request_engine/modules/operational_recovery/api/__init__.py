e4a10987ec3c849325e991620b33f71c56ece550
JulioMarte <julio19955@hotmail.com>
2026-08-30 18:30:49 -0400

    Replace recovery tuple wiring with typed runtime

--- src/request_engine/modules/operational_recovery/api/__init__.py
+++ src/request_engine/modules/operational_recovery/api/__init__.py
@@ -4,53 +4,34 @@ from request_engine.modules.booking.contracts.recovery import RecoveryBookingPor
 from request_engine.modules.booking.contracts.recovery_schedule import (
     RecoveryAssignmentSchedulePort,
 )
-from request_engine.modules.communications.contracts.recovery import (
-    RecoveryCommunicationPort,
-)
+from request_engine.modules.communications.contracts.recovery import RecoveryCommunicationPort
 from request_engine.modules.live_capacity.contracts.recovery import RecoveryCapacitySource
 from request_engine.modules.operational_recovery.adapters.db.recovery_autonomy_policy_store import (
     PostgresRecoveryAutonomyPolicyStore,
 )
-from request_engine.modules.operational_recovery.adapters.db.store import (
-    PostgresRecoveryRepository,
-)
+from request_engine.modules.operational_recovery.adapters.db.store import PostgresRecoveryRepository
 from request_engine.modules.operational_recovery.adapters.db.workflow_repository import (
     PostgresRecoveryWorkflowRepository,
 )
-from request_engine.modules.operational_recovery.api.errors import (
-    operational_recovery_error_handler,
-)
+from request_engine.modules.operational_recovery.api.errors import operational_recovery_error_handler
 from request_engine.modules.operational_recovery.api.router import create_router
+from request_engine.modules.operational_recovery.api.runtime import OperationalRecoveryRuntime
 from request_engine.modules.operational_recovery.api.workflow_autonomy_router import (
     create_autonomy_router,
 )
 from request_engine.modules.operational_recovery.api.workflow_communication_router import (
     create_communication_router,
 )
-from request_engine.modules.operational_recovery.api.workflow_errors import (
-    workflow_recovery_error_handler,
-)
-from request_engine.modules.operational_recovery.api.workflow_reschedule_router import (
-    create_reschedule_router,
-)
-from request_engine.modules.operational_recovery.api.workflow_router import (
-    create_workflow_router,
-)
-from request_engine.modules.operational_recovery.application.errors import (
-    OperationalRecoveryError,
-)
-from request_engine.modules.operational_recovery.application.service import (
-    OperationalRecoveryService,
-)
-from request_engine.modules.operational_recovery.application.workflow_intake_port import (
-    RecoveryIntakeControlPort,
-)
+from request_engine.modules.operational_recovery.api.workflow_errors import workflow_recovery_error_handler
+from request_engine.modules.operational_recovery.api.workflow_reschedule_router import create_reschedule_router
+from request_engine.modules.operational_recovery.api.workflow_router import create_workflow_router
+from request_engine.modules.operational_recovery.application.errors import OperationalRecoveryError
+from request_engine.modules.operational_recovery.application.service import OperationalRecoveryService
+from request_engine.modules.operational_recovery.application.workflow_intake_port import RecoveryIntakeControlPort
 from request_engine.modules.operational_recovery.application.workflow_location_port import (
     RecoveryLocationExtensionPort,
 )
-from request_engine.modules.operational_recovery.application.workflow_service import (
-    RecoveryWorkflowService,
-)
+from request_engine.modules.operational_recovery.application.workflow_service import RecoveryWorkflowService
 from request_engine.modules.operational_recovery.contracts.workflow import (
     RecoveryActionConflict,
     RecoveryIncidentNotFound,
@@ -59,6 +40,8 @@ from request_engine.modules.operational_recovery.contracts.workflow import (
 )
 from request_engine.platform.db.session import SessionFactory
 from request_engine.platform.security.http import ActorResolver
+
+__all__ = ["OperationalRecoveryRuntime", "install_http"]
 
 
 def install_http(
@@ -72,7 +55,7 @@ def install_http(
     intake: RecoveryIntakeControlPort,
     location_schedule: RecoveryLocationExtensionPort,
     assignment_schedule: RecoveryAssignmentSchedulePort,
-) -> tuple[OperationalRecoveryService, RecoveryWorkflowService]:
+) -> OperationalRecoveryRuntime:
     proposal_repository = PostgresRecoveryRepository(session_factory)
     service = OperationalRecoveryService(
         repository=proposal_repository,
@@ -105,4 +88,4 @@ def install_http(
     app.include_router(
         create_autonomy_router(PostgresRecoveryAutonomyPolicyStore(session_factory), actor_resolver)
     )
-    return service, workflow
+    return OperationalRecoveryRuntime(service=service, workflow=workflow)
