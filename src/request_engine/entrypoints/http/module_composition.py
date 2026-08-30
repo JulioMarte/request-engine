d9dd8e5a35665b0d179f94c7dbc241980cd0116c
JulioMarte <julio19955@hotmail.com>
2026-08-30 17:50:46 -0400

    Minimize F6 extend-day composition diff

--- src/request_engine/entrypoints/http/module_composition.py
+++ src/request_engine/entrypoints/http/module_composition.py
@@ -1,33 +1,52 @@
 from fastapi import FastAPI
 
+from request_engine.bootstrap.recovery_catalog import CatalogRecoveryLocationAdapter
+from request_engine.bootstrap.recovery_queue import QueueRecoveryIntakeAdapter
 from request_engine.modules.booking.api import install_http as install_booking_http
-from request_engine.modules.booking.api.live_capacity import build_live_capacity_source as build_booking_live_capacity_source
+from request_engine.modules.booking.api.live_capacity import (
+    build_live_capacity_source as build_booking_live_capacity_source,
+)
 from request_engine.modules.booking.api.recovery import build_recovery_booking_port
-from request_engine.modules.booking.api.recovery_schedule import build_recovery_assignment_schedule_port
+from request_engine.modules.booking.api.recovery_schedule import (
+    build_recovery_assignment_schedule_port,
+)
 from request_engine.modules.catalog.api import install_http as install_catalog_http
-from request_engine.modules.catalog.api.recovery import build_recovery_location_schedule_port
+from request_engine.modules.catalog.api.recovery_schedule import (
+    build_recovery_location_schedule_port,
+)
 from request_engine.modules.communications.api import install_http as install_communications_http
-from request_engine.modules.communications.api.recovery import build_recovery_communication_port
-from request_engine.modules.discovery.api import install_http as install_discovery_http
+from request_engine.modules.communications.api.recovery import (
+    build_recovery_communication_port,
+)
+from request_engine.modules.delivery.api import install_http as install_delivery_http
+from request_engine.modules.delivery.api.live_capacity import (
+    build_live_capacity_source as build_delivery_live_capacity_source,
+)
 from request_engine.modules.live_capacity.api import install_http as install_live_capacity_http
 from request_engine.modules.live_capacity.api.recovery import build_recovery_capacity_source
 from request_engine.modules.operational_copilot.api import (
     build_live_capacity_at_risk_reader,
+)
+from request_engine.modules.operational_copilot.api import (
     install_http as install_copilot_http,
 )
 from request_engine.modules.operational_recovery.api import install_http as install_recovery_http
-from request_engine.modules.operational_recovery.api.queries import build_recovery_proposal_reader
-from request_engine.modules.operational_recovery.api.workflow_adapters import (
-    CatalogRecoveryLocationAdapter,
-    QueueRecoveryIntakeAdapter,
+from request_engine.modules.operational_recovery.api.proposal_reader import (
+    build_recovery_proposal_reader,
 )
+from request_engine.modules.queue.api import QueueSlotOfferHttpPorts
 from request_engine.modules.queue.api import install_http as install_queue_http
-from request_engine.modules.queue.api.live_capacity import build_live_capacity_source as build_queue_live_capacity_source
+from request_engine.modules.queue.api.live_capacity import (
+    build_live_capacity_source as build_queue_live_capacity_source,
+)
 from request_engine.modules.queue.api.recovery import build_recovery_intake_control_port
-from request_engine.modules.tenancy.api.authority import build_operational_authority_party_reader
+from request_engine.modules.requests.api import install_http as install_requests_http
+from request_engine.modules.tenancy.api import (
+    build_operational_authority_party_reader,
+    build_party_authority_reader,
+)
 from request_engine.platform.db.session import SessionFactory
 from request_engine.platform.security.http import ActorResolver
-from request_engine.modules.delivery.api.live_capacity import build_live_capacity_source as build_delivery_live_capacity_source
 
 
 def install_business_modules(
@@ -35,11 +54,30 @@ def install_business_modules(
     *,
     session_factory: SessionFactory,
     actor_resolver: ActorResolver,
+    slot_offer_ports: QueueSlotOfferHttpPorts | None,
+    appointment_option_signing_key: bytes,
 ) -> None:
+    party_authority_reader = build_party_authority_reader(session_factory)
+    install_requests_http(app, session_factory=session_factory, actor_resolver=actor_resolver)
     install_catalog_http(app, session_factory=session_factory, actor_resolver=actor_resolver)
-    install_booking_http(app, session_factory=session_factory, actor_resolver=actor_resolver)
-    install_queue_http(app, session_factory=session_factory, actor_resolver=actor_resolver)
-    install_discovery_http(app, session_factory=session_factory, actor_resolver=actor_resolver)
+    install_booking_http(
+        app,
+        session_factory=session_factory,
+        actor_resolver=actor_resolver,
+        party_authority_reader=party_authority_reader,
+        appointment_option_signing_key=appointment_option_signing_key,
+    )
+    install_queue_http(
+        app,
+        session_factory=session_factory,
+        actor_resolver=actor_resolver,
+        slot_offer_ports=slot_offer_ports,
+    )
+    install_delivery_http(
+        app,
+        session_factory=session_factory,
+        actor_resolver=actor_resolver,
+    )
     booking_capacity = build_booking_live_capacity_source()
     queue_capacity = build_queue_live_capacity_source()
     delivery_capacity = build_delivery_live_capacity_source()
