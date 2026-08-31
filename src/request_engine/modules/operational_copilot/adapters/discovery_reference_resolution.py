861db75e4401e33dcf2cf10f412e3d83619830a3
JulioMarte <julio19955@hotmail.com>
2026-08-30 23:02:31 -0400

    Mark natural discovery publication as immediate

--- src/request_engine/modules/operational_copilot/adapters/discovery_reference_resolution.py
+++ src/request_engine/modules/operational_copilot/adapters/discovery_reference_resolution.py
@@ -43,4 +43,5 @@ async def resolve_publish_discovery(
         effective_start=clock.observed_at,
         resource_id=resource.resource_id,
         provider_visibility="public",
+        effective_start_origin="resolved_now",
     )
