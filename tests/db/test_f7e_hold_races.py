536b9a26dc987e529aa071b00430d91e62df4162
JulioMarte <julio19955@hotmail.com>
2026-09-02 15:19:49 -0400

    Sort F7e hold-race imports

--- tests/db/test_f7e_hold_races.py
+++ tests/db/test_f7e_hold_races.py
@@ -2,9 +2,9 @@ import asyncio
 from uuid import UUID, uuid4
 
 import pytest
-
 from f7e_selection_fixture import F7eSelectionFixture, PgConnection, create_f7e_selection_fixture
 from f7e_selection_race_barrier import AsyncTwoPartyBarrier, gated_lock
+
 from request_engine.modules.queue.adapters.db import recall_hold as recall_hold_module
 from request_engine.modules.queue.adapters.db.same_day_selection_commands import (
     PostgresSameDaySelectionCommands,
