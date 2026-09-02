c4f17e35c4dd4ec136c87fdfaedad78c59b94118
JulioMarte <julio19955@hotmail.com>
2026-09-02 16:29:54 -0400

    Scope portable Party profiles by publisher

--- migrations/s0d_steps/step_0028_profile_provenance.py
+++ migrations/s0d_steps/step_0028_profile_provenance.py
@@ -1,4 +1,4 @@
-"""Attach portable profile snapshots to their publishing organization."""
+"""Attach portable Party profile snapshots to their publishing organization."""
 
 from alembic import op
 
@@ -6,15 +6,15 @@ _SQL = r"""
 SET ROLE request_engine_schema_owner;
 SET search_path = request_engine, pg_catalog;
 
-ALTER TABLE request_engine.portable_person_profiles
+ALTER TABLE request_engine.portable_party_profiles
     ADD COLUMN publisher_organization_id uuid NOT NULL
         REFERENCES request_engine.organizations(id);
-ALTER TABLE request_engine.portable_person_profiles
-    DROP CONSTRAINT portable_person_profiles_pkey;
-ALTER TABLE request_engine.portable_person_profiles
-    ADD PRIMARY KEY (portable_person_id, publisher_organization_id);
-CREATE INDEX portable_person_profiles_person_idx
-    ON request_engine.portable_person_profiles(portable_person_id)
+ALTER TABLE request_engine.portable_party_profiles
+    DROP CONSTRAINT portable_party_profiles_pkey;
+ALTER TABLE request_engine.portable_party_profiles
+    ADD PRIMARY KEY (portable_party_id, publisher_organization_id);
+CREATE INDEX portable_party_profiles_party_idx
+    ON request_engine.portable_party_profiles(portable_party_id)
     WHERE active;
 
 RESET ROLE;
