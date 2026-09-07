870482ad1894861cf09ef4eaab413537f4461c3c
JulioMarte <julio19955@hotmail.com>
2026-09-07 15:42:43 -0400

    test(api): include operation catalog in current HTTP inventory

--- tests/e2e/http_surface_current.py
+++ tests/e2e/http_surface_current.py
@@ -9,6 +9,7 @@ from .http_surface_f6 import F6_HTTP_OPERATIONS
 from .http_surface_f6_tools import F6_TOOL_HTTP_OPERATIONS
 from .http_surface_f7 import F7_HTTP_OPERATIONS
 from .http_surface_onboarding import ONBOARDING_HTTP_OPERATIONS
+from .http_surface_operation_catalog import OPERATION_CATALOG_HTTP_OPERATIONS
 from .http_surface_s0b import S0B_HTTP_OPERATIONS
 from .http_surface_s0b2 import S0B2_HTTP_OPERATIONS
 from .http_surface_s0c import S0C_HTTP_OPERATIONS
@@ -31,6 +32,7 @@ PUBLIC_HTTP_OPERATIONS: tuple[PublicHttpOperation, ...] = (
     + DAY_BOARD_HTTP_OPERATIONS
     + S5_HTTP_OPERATIONS
     + ONBOARDING_HTTP_OPERATIONS
+    + OPERATION_CATALOG_HTTP_OPERATIONS
 )
 
 MATRIX_OPERATIONS: tuple[PublicHttpOperation, ...] = (
