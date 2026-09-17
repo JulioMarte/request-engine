7272a19efaf7943305d2d47f729fd5493b617355
JulioMarte <julio19955@hotmail.com>
2026-09-17 10:22:38 -0400

    Match runtime login grants to current trust boundaries

--- deploy/reference/init-runtime-roles.sql
+++ deploy/reference/init-runtime-roles.sql
@@ -5,7 +5,10 @@ DO $$ BEGIN
  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname='request_e2e_read') THEN CREATE ROLE request_e2e_read LOGIN; END IF;
  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname='request_e2e_control') THEN CREATE ROLE request_e2e_control LOGIN; END IF;
 END $$;
-GRANT request_app TO request_e2e_app;
-GRANT request_worker TO request_e2e_worker;
-GRANT request_read TO request_e2e_read;
+GRANT request_engine_app TO request_e2e_app;
+GRANT request_engine_worker TO request_e2e_worker;
 GRANT request_platform_control TO request_e2e_control;
+GRANT EXECUTE ON FUNCTION request_platform.read_principal_authority(uuid) TO request_e2e_read;
+GRANT EXECUTE ON FUNCTION request_platform.read_platform_provisioners(uuid,uuid,integer) TO request_e2e_read;
+GRANT EXECUTE ON FUNCTION request_platform.read_identity_recovery_cases(uuid,uuid,integer) TO request_e2e_read;
+GRANT EXECUTE ON FUNCTION request_platform.read_native_identities(uuid,uuid,integer) TO request_e2e_read;
